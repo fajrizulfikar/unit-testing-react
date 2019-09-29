8a56d9eb2fc5003674eca6392f9c66a3397ec5cf
zulfikar fajri <fajrizulfikar85@gmail.com>
2019-09-29 15:57:33 +0700

    change test title

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import '@testing-library/jest-dom/extend-expect';
 
 import App from './App';
 
-describe("Test Toggle using Enzyme", () => {    
+describe("Test Toggle using React Testing Library", () => {    
   afterEach(cleanup);
   
   it("render Toggle component", () => {
